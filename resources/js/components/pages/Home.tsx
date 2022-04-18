f73611af519b1be6912ed7b802dfeb4888d33e72
nkwtnb <n.watanabe0305@gmail.com>
2022-04-18 17:41:56 +0900

    非ログイン時のメッセージ表示

--- resources/js/components/pages/Home.tsx
+++ resources/js/components/pages/Home.tsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useLayoutEffect } from 'react';
 import NewPost from '../organisms/NewPost';
 import TImeline from '../organisms/TImeline';
 import { AuthContext } from "../../Context";
+import { Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default () => {
   const {authState, setAuthState} = useContext(AuthContext);
@@ -9,8 +11,13 @@ export default () => {
     <>
       <div className='offset-md-2 col-md-8'>
         {
-          authState.name !== "" &&
-          <NewPost placeholder='ひとりごとをつぶやく...'/>
+          authState.name !== ""
+          ?
+            <NewPost placeholder='ひとりごとをつぶやく...'/>
+          :
+            <div className="alert alert-warning" role="alert">
+              <b>投稿</b>、<b>コメント</b>、<b>いいね</b>などのアクションをするには、<Link to="/login">ログイン</Link>が必要です。
+            </div>
         }
         <TImeline name="" filter="post"/>
       </div>
