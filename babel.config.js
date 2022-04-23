febe31ebcbbff7e6b9af61404e4bcac0881c482b
nkwtnb <n.watanabe0305@gmail.com>
2022-04-23 22:57:38 +0900

    add test

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+				"targets": {
+					"node": true
+				}
+			}
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+  ]
+}
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // "moduleFileExtensions": ["js", "jsx", "ts", "tsx"],
+  "moduleNameMapper": {
+    // "^@api$": "<rootDir>/js/api",
+    '^@api/(.+)': '<rootDir>/resources/js/api/$1',
+  },
+}
